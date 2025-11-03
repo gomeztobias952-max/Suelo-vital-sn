4a1bf7c6bf74f27d19f511a95b5503c4fac67b03
gomeztobias952-max <gomeztobias952@gmail.com>
2025-11-03 16:03:50 -0300

    Update Suelo-vital-sn.ino

--- Suelo-vital-sn.ino
+++ Suelo-vital-sn.ino
@@ -2,8 +2,8 @@
 
 // ---------------- CONFIGURACIÓN DEL ESP ----------------
 SoftwareSerial esp(2, 3);  // RX Arduino(2) <-- TX ESP | TX Arduino(3) --> RX ESP
-const char* WIFI_SSID = "Familia Gomez -2.4GHz";  // ⚠️ tu red WiFi exacta
-const char* WIFI_PASS = "2219427800";             // ⚠️ contraseña exacta
+const char* WIFI_SSID = "claudio";  // ⚠️ tu red WiFi exacta
+const char* WIFI_PASS = "123456789";             // ⚠️ contraseña exacta
 
 // Canal ThingSpeak
 String writeAPIKey = "8XBJK1GAW61UV11Q";
